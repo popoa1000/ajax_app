a15047531305e2992bfb0a2ba41b8c0c44847308
popoa1000 <gecko.ekoda@gmail.com>
2021-01-13 21:01:20 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
+  get 'posts', to: 'posts#checked'
   #iminashi
 end
