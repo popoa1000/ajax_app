43c86538dd247dc7c72343bc8d4f47aab4ce324c
popoa1000 <gecko.ekoda@gmail.com>
2021-01-13 21:05:42 +0900

    Revert "既読機能のサーバーサイド実装"
    This reverts commit a15047531305e2992bfb0a2ba41b8c0c44847308.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   # [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
   root to: 'posts#index'
   post 'posts', to: 'posts#create'
-  get 'posts', to: 'posts#checked'
   #iminashi
 end
